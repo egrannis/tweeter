4cca2a636bd1d9e227e46b581206e1e4ce800ee0
Emma Grannis <grannis.emma@gmail.com>
2022-04-27 17:24:43 +0000

    Implement functioning counter that turns ref when over character limit, and turns back to grey when it goes back under limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,24 +1,29 @@
 $(document).ready(function() {
- console.log('Working');
- $("#tweet-text").on('keypress', function(){
-   console.log(140-$(this).val().length);
-   //make sure youre accessing correct element
-   console.log($(this));
-   console.log($(this).children(".counter"));
- console.log($(this).parent()); //.children(".counter")); // access selector like you would in css, eg . for class, # for id, etc.
- // see how you can access the value in the element
- 
-  })
-}
-);
+  $("#tweet-text").on('keyup', function() {
+    const counter = 140;
+    let count = this.value.length;
+    $("#counter").text(counter - count);
+    if ((counter - count) < 0) {
+      $("#counter").addClass("red");
+    } else {
+      $("#counter").removeClass("red");
+    }
+  });
+});
 
-
-
+// CLEAN UP LATER - KEEP FOR NOW
 // Form  for today's activities
 // Find the form-input field with jquery ( or vanillaJS)
-// Add event listner that is able 
+// Add event listner that is able
 //to run on every letter typed in
-// use that to get the output of what was 
+// use that to get the output of what was
 // typed in
 // 140 - what was typed in = how many characters are left
 // display that number on the webpage
+
+// using this - and then traversing - return to???
+//make sure youre accessing correct element
+//    console.log($(this));
+//    console.log($(this).children(".counter"));
+//  console.log($(this).parent()); //.children(".counter")); // access selector like you would in css, eg . for class, # for id, etc.
+// see how you can access the value in the element
