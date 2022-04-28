fd241b3785fd86a30015cb15b7e817aa408e095c
Emma Grannis <grannis.emma@gmail.com>
2022-04-28 00:15:13 +0000

    Initial HTML written into file within document.ready function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,29 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//input: tweet object. output: tweet article with HTML structure
+$(document).ready(function() {
+  
+  const createTweetElement = function(tweetObj) {
+    let $tweet = $(`<section class="tweet">
+      <header class="user-data">
+        <div class="lefthand">
+          <img src='${tweetObj.user.avatars}'class="avatar"></img>
+          <div class="name">${tweetObj.user.name}</div>
+        </div>
+      <div class="handle">${tweetObj.user.handle}</div>
+    </header>
+    <article class="tweet-post">${tweetObj.content.text}</article>
+    <footer>
+      <div>${tweetObj.created_at}</div> // how to convert this date properly?
+      <div class="icons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </section>`);
+  }
+return $tweet;
+//do something to get this to populate?
+});
