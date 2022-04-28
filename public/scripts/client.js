606794e713ee236da4a47df8e7cbcf250674f5d0
Emma Grannis <grannis.emma@gmail.com>
2022-04-28 20:08:03 +0000

    Implemnet get request function, add fail condition in which error status pops up

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
 $(document).ready(function() {
 
   //takes in tweet object as input, and outputs tweet article with HTML structure
@@ -19,7 +18,7 @@ $(document).ready(function() {
     </header>
     <article class="tweet-post">${tweetObj.content.text}</article>
     <footer>
-      <div>${tweetObj.created_at}</div> 
+      <div>${timeago.format(tweetObj.created_at)}</div> 
       <div class="icons">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -37,84 +36,31 @@ const renderTweets = function(tweets) {
   }
 };
 
-// Example Data to test renderTweets function
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1650931475287
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1651017875287
-  }
-]
-
-renderTweets(data);
-
-// // Test / driver code (temporary). Eventually will get this from the server.
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
-
-// const $tweet = createTweetElement(tweetData);
-
-// // Test / driver code (temporary)
-// console.log('$tweet: ', $tweet); // to see what it looks like
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-
 // listener for submission of tweet form
 $(".tweet-form").on("submit", function (event) {
   event.preventDefault(); // prevents default form submission behaviour
   const serialData = $("#tweet-text").serialize(); // serialize the form data
 
-  //submit a post request that sends the serialized data to the server
-  //BIG question for line 95. Did I do this wrong? Do I need to do $.ajax (url, method: POST, etc??) This is a shorthand Ajax function, which is equivalent to:
-  /* $.ajax({
-  url: url,
-  data: data,
-  success: success,
-  dataType: dataType
-}); */
-
   $.post("/tweets", serialData, function () { // $.post (URL, Data, callback) // when I googled ajax post I found this but unsure if I should do .ajax?
-    console.log("successful post")
+    console.log("successful post");
+    loadTweets();
   })
+   .fail(function (error) {
+    alert(error.status + ": " + error.statusText);
+    })
 })
 
 // fetches tweets from the tweets page
 const loadTweets = function () {
-  $.get("/tweets", serialData, function () {
-    console.log('It works?')
+  $.get("/tweets", function (response) {
+  console.log(response);
+  renderTweets(response);
   })
+  .fail(function (error) {
+   alert(error.status + ": " + error.statusText);
+   })
 }
 
-// unsure if this below is better than on line 108
-const loadTweets = function () {
-  $.ajax("/tweets", { method: 'GET'})
-  .then(renderTweets)
-  .then(console.log("Help"));
+loadTweets();
 
 }); //end of document.ready function
